a252aed7aee2682e6a84ead8fbfe7e599fe39956
yashveerthakur17 <yashveerthakur17@gmail.com>
2025-06-10 01:38:44 +0530

    adjusted the test file

--- test/test_op.py
+++ test/test_op.py
@@ -2,21 +2,21 @@ from src.math_op import add,sub,mul,div
 
 def test_add():
     assert add(1,2) == 3
-    assert add(3,2) == 4
-    assert add(4,2) == 5
+    assert add(3,2) == 5
+    assert add(4,2) == 6
 
 def test_sub():
-    assert sub(1,2) == 3
-    assert sub(3,2) == 4
-    assert sub(4,2) == 5
+    assert sub(1,2) == -1
+    assert sub(3,2) == 1
+    assert sub(4,2) == 2
 
 def test_mul():
-    assert mul(1,2) == 3
-    assert mul(3,2) == 4
-    assert mul(4,2) == 5
+    assert mul(1,2) == 2
+    assert mul(3,2) == 6
+    assert mul(4,2) == 8
 
 def test_div():
-    assert div(1,2) == 3
-    assert div(3,2) == 4
-    assert div(4,2) == 5
+    assert div(2,2) == 1
+    assert div(10,2) == 5
+    assert div(30,3) == 10
 
